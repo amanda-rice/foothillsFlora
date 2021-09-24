a65d330ed9cabbb1951d3adb67a8d8e16d5981ad
amanda rice <ariceburner@gmail.com>
2021-09-23 20:07:18 -0600

    initial setup

--- foothillsFlora.client/src/env.js
+++ foothillsFlora.client/src/env.js
@@ -1,6 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const domain = 'dev-lh6ggyff.us.auth0.com'
+export const audience = 'https://Auth-Todo.com'
+export const clientId = 'HzQgoIex6tKEA7CvyOwT0dUlaesDjLt2'
